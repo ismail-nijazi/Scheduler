2e334dc485fe7c3bed819f0baa8ed9523e818604
ismail-nijazi <ismail.nijazi123@gmail.com>
2022-10-07 10:47:28 +0200

    searh on project view

--- src/views/ProjectView.js
+++ src/views/ProjectView.js
@@ -1,106 +1,114 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux';
 import { sortTasks, getProjects, getTasks } from '../store/slices/tasks';
 import { useNavigate } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 import SearchHeader from '../components/SearchHeader';
-import Task from '../components/Task'
+import Task from '../components/Task';
 import PopupQuestion from '../components/PopupQuestion';
-import { projects,tasks } from "../services/database";
+import { projects, tasks } from '../services/database';
 
 function ProjectView() {
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
-	const tasksStore = useSelector(state => state.tasks);
- 	const [tasksList, setTasks] = useState(tasksStore.tasksPerProject.tasks);
-	const [deleteConfirm, showConfirmation] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [typingTimeout, setTypingTimeout] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const tasksStore = useSelector((state) => state.tasks);
+  const [tasksList, setTasks] = useState(tasksStore.tasksPerProject.tasks);
+  const [deleteConfirm, showConfirmation] = useState(false);
 
-	useEffect(() => {
-		setTasks(tasksStore.tasksPerProject.tasks);
-	},[tasksStore.tasksPerProject])
-	
-	const onSearch = (event) => {
-		const searchPhrase = event.target.value.toLowerCase();
-		const filteredTasks = tasksStore.tasksPerProject.tasks.filter((task) => {
-			if (
-				task.description.toLowerCase().includes(searchPhrase)
-				// ||task.category_id.toLowerCase().includes(searchPhrase)
-			) {
-				return true;
-			}
-			return false;
-		});
-		setTasks(filteredTasks);
-	}
+  useEffect(() => {
+    setTasks(tasksStore.tasksPerProject.tasks);
+  }, [tasksStore.tasksPerProject]);
 
-	const renderOptions = () => {
-		return (
-			<>
-				<button
-					type="button"
-					className="btn option"
-					onClick={() => navigate("/project/edit")}
-				>
-				Edit				
-			</button>
-			<button
-				type="button"
-				className="btn option"
-				onClick={() => showConfirmation(true)}
-			>
-				Delete			
-			</button>
-		</>)
-	}
+  const search = (searchPhrase) => {
+    setTypingTimeout(
+      setTimeout(() => {
+        const filteredTasks = tasksStore.tasksPerProject.tasks.filter((task) => {
+          if (task.description.toLowerCase().includes(searchPhrase)) {
+            return true;
+          }
+          return false;
+        });
+        setTasks(filteredTasks);
+        setLoading(false);
+      }, 500)
+    );
+  };
 
-	return (
-		<div className="page-container tasks-view">
-			<PopupQuestion
-				title={"Delete task"}
-				alert={
-					<span
-						className="warning"
-					>
-						All tasks in this project will be removed!
-					</span>}
-				text="Are you sure you want to delet this project?"
-				visible={deleteConfirm}
-				onClose={() => showConfirmation(false)}
-				confirm={async () => {
-					await tasks.removeProjectsTasks(
-						tasksStore.tasksPerProject.project.id
-					);
-					await projects.remove(tasksStore.tasksPerProject.project.id);
-					getProjects(dispatch);	
-					getTasks(dispatch);	
-					navigate("/tasks");
-					}
-				}
-				cancle={() => showConfirmation(false)}
-			/>
-			<SearchHeader
-				title={tasksStore.tasksPerProject?.project?.name || "Tasks"}
-				onSearch={onSearch}
-				sort={(sortBy) => setTasks(sortTasks(sortBy,tasks))}
-				options={renderOptions()}
-				newTaskRoute="/project/new-task"
-			/>
-			{tasksStore.loading ?
-				<main className="spinner-container">
-					<Spinner className="spinner-medium"/>
-				</main> :
-				<main>
-					{ tasksList.length > 0 ? 
-						tasksList.map((task, index) => <Task task={task} key={index} />) :
-						<div className="no-tasks">
-							<img src={require("../assets/images/no-tasks.png")} />
-							<span>No tasks found...</span>
-						</div>
-					}
-				</main>
-			}
-		</div>
-	)
+  const onSearch = (searchPhrase) => {
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+    if (!loading) {
+      setLoading(true);
+    }
+    search(searchPhrase);
+  };
+
+  const renderOptions = () => {
+    return (
+      <>
+        <button
+          type="button"
+          className="btn option"
+          onClick={() => navigate('/project/edit')}>
+          Edit
+        </button>
+        <button
+          type="button"
+          className="btn option"
+          onClick={() => showConfirmation(true)}>
+          Delete
+        </button>
+      </>
+    );
+  };
+
+  return (
+    <div className="page-container tasks-view">
+      <PopupQuestion
+        title={'Delete task'}
+        alert={
+          <span className="warning">All tasks in this project will be removed!</span>
+        }
+        text="Are you sure you want to delet this project?"
+        visible={deleteConfirm}
+        onClose={() => showConfirmation(false)}
+        confirm={async () => {
+          await tasks.removeProjectsTasks(tasksStore.tasksPerProject.project.id);
+          await projects.remove(tasksStore.tasksPerProject.project.id);
+          getProjects(dispatch);
+          getTasks(dispatch);
+          navigate('/tasks');
+        }}
+        cancle={() => showConfirmation(false)}
+      />
+      <SearchHeader
+        title={tasksStore.tasksPerProject?.project?.name || 'Tasks'}
+        onSearch={onSearch}
+        sort={(sortBy) => setTasks(sortTasks(sortBy, tasks))}
+        options={renderOptions()}
+        newTaskRoute="/project/new-task"
+      />
+      {tasksStore.loading || loading ? (
+        <main className="spinner-container">
+          <Spinner className="spinner-medium" />
+        </main>
+      ) : (
+        <main>
+          {tasksList.length > 0 ? (
+            tasksList.map((task, index) => <Task task={task} key={index} />)
+          ) : (
+            <div className="no-tasks">
+              <img src={require('../assets/images/no-tasks.png')} />
+              <span>No tasks found...</span>
+            </div>
+          )}
+        </main>
+      )}
+    </div>
+  );
 }
 
 export default ProjectView;
--- src/views/TasksView.js
+++ src/views/TasksView.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { sortTasks } from '../store/slices/tasks';
 import Task from '../components/Task';
@@ -6,69 +6,66 @@ import SearchHeader from '../components/SearchHeader';
 import Spinner from '../components/Spinner';
 
 function TasksView() {
-	const tasksStore = useSelector(state => state.tasks)
-	const [tasks, setTasks] = useState(tasksStore.tasks);
-	const [typingTimeout, setTypingTimeout] = useState(0);
-	const [loading, setLoading] = useState(false);
+  const tasksStore = useSelector((state) => state.tasks);
+  const [tasks, setTasks] = useState(tasksStore.tasks);
+  const [typingTimeout, setTypingTimeout] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-	useEffect(() => {
-		setTasks(tasksStore.tasks);
-	}, [tasksStore.tasks]);
+  useEffect(() => {
+    setTasks(tasksStore.tasks);
+  }, [tasksStore.tasks]);
 
+  const search = (searchPhrase) => {
+    setTypingTimeout(
+      setTimeout(() => {
+        const filteredTasks = tasksStore.tasks.filter((task) => {
+          if (task.description.toLowerCase().includes(searchPhrase)) {
+            return true;
+          }
+          return false;
+        });
+        setTasks(filteredTasks);
+        setLoading(false);
+      }, 500)
+    );
+  };
 
-	const search = (searchPhrase) => {
-		setTypingTimeout(setTimeout(() => {
-			const filteredTasks = tasksStore.tasks.filter((task) => {
-				if (
-					task.description.toLowerCase().includes(searchPhrase)
-						// ||task.category_id.toLowerCase().includes(searchPhrase)
-					) {
-						return true;
-					}
-						return false;
-				}
-			);
-			console.log(filteredTasks.length)
-			setTasks(filteredTasks);
-			setLoading(false);
-		}, 1000))
-	}
+  const onSearch = (searchPhrase) => {
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+    if (!loading) {
+      setLoading(true);
+    }
+    search(searchPhrase);
+  };
 
-	const onSearch = (searchPhrase) => {
-		if (typingTimeout) {
-			clearTimeout(typingTimeout);
-		}
-		if (!loading) {
-			setLoading(true);
-		}
-		search(searchPhrase);
-	}
-
-	return (
-		<div className="page-container tasks-view">
-			<SearchHeader
-				onSearch={onSearch}
-				title="Tasks"
-				sort={(sortBy) => setTasks(sortTasks(sortBy,tasks))}
-				newTaskRoute="/task/new"
-			/>
-			{tasksStore.loading || loading ?
-				<main className="spinner-container">
-					<Spinner className="spinner-medium"/>
-				</main> :
-				<main>
-					{ tasks.length > 0 ? 
-						tasks.map((task, index) => <Task task={task} key={index} />) :
-						<div className="no-tasks">
-							<img src={require("../assets/images/no-tasks.png")} />
-							<span>No tasks found...</span>
-						</div>
-					}
-				</main>
-			}
-			
-		</div>
-	)
+  return (
+    <div className="page-container tasks-view">
+      <SearchHeader
+        onSearch={onSearch}
+        title="Tasks"
+        sort={(sortBy) => setTasks(sortTasks(sortBy, tasks))}
+        newTaskRoute="/task/new"
+      />
+      {tasksStore.loading || loading ? (
+        <main className="spinner-container">
+          <Spinner className="spinner-medium" />
+        </main>
+      ) : (
+        <main>
+          {tasks.length > 0 ? (
+            tasks.map((task, index) => <Task task={task} key={index} />)
+          ) : (
+            <div className="no-tasks">
+              <img src={require('../assets/images/no-tasks.png')} />
+              <span>No tasks found...</span>
+            </div>
+          )}
+        </main>
+      )}
+    </div>
+  );
 }
 
-export default TasksView
+export default TasksView;
